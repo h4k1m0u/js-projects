315effd51e8216590e8b55d560aee79b5a8ba965
h4k1m0u <h.benoudjit@gmail.com>
2020-06-14 19:01:23 +0200

    Navigation controls & add points to scene

--- three-project/src/app.js
+++ three-project/src/app.js
@@ -1,16 +1,28 @@
 // source: https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 
 
+/*
+* Scene
+*/
 // setup scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,
     0.1, 1000);
-camera.position.z = 5;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+// scene navigation with pan/zoom/rotate
+const controls = new OrbitControls(camera, renderer.domElement);
+camera.position.z = 5;
+controls.update();
+
+
+/*
+* Objects
+*/
 // add axes to scene
 const axes = new THREE.AxesHelper(5);
 scene.add(axes);
@@ -19,20 +31,37 @@ scene.add(axes);
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({color: 0x00aaff});
 const cube = new THREE.Mesh(geometry, material);
+cube.position.x = 3;
 scene.add(cube);
 
+// add points to scene
+const geometry_points = new THREE.Geometry();
+const coords = [[0, 0, 0], [1, 0, 0], [1, 1, 0], [0, 1, 0]];
+geometry_points.vertices = coords.map(coord => new THREE.Vector3(coord[0], coord[1], coord[2]));
+const material_points = new THREE.PointsMaterial({size: 0.2, color: 0xffffff});
+const mesh_points = new THREE.Points(geometry_points, material_points);
+scene.add(mesh_points);
+
+
+/*
+* Loop
+*/
 // animation loop
 let start = Date.now();
 
 const animate = function() {
-    // rotate cube in each frame
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    // update orbit controls in each frame
+    controls.update();
+
+    // rotate cube in each frame (for 10s)
+    if (Date.now() - start < 10000) {
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+    }
     renderer.render(scene, camera);
 
-    // called before next screen repaint (for 10s)
-    if (Date.now() - start < 10000)
-        window.requestAnimationFrame(animate);
+    // called before next screen repaint
+    window.requestAnimationFrame(animate);
 }
 
 animate();
