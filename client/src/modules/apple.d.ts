36903fc8834676b7ef64d0dd399513a13e8ae1e2
h4k1m0u <h.benoudjit@gmail.com>
2021-05-15 00:43:20 +0200

    Stop YCM/tsserver lint err about inexisting module

---
+++ client/src/modules/apple.d.ts
@@ -0,0 +1,5 @@
+// equivalent of header file in C/C++
+// `declare` specify type of an existing variable that can be referenced (i.e. module to import here)
+// i.e. concretely used so YCM/tsserver won't complain when importing *.png image
+// see: https://www.typescriptlang.org/docs/handbook/modules.html#wildcard-module-declarations
+declare module '*.png';
